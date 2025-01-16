caaebaab8ae5a7cd23cb63f7a18599ad501317b4
onlybandoption <dachserdachserintelligent@gmail.com>
2025-01-16 15:05:38 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3545607573"
+    serviceWorkerVersion: "2762038429"
   }
 });
