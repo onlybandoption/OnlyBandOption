8f88dd6a30e2a9faf8bc797d83ea6de0ae0bc555
onlybandoption <dachserdachserintelligent@gmail.com>
2026-07-23 18:08:13 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2669811248" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "434448502" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
